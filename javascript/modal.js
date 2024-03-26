308b7dc8cb4dd87154b3bb2c5086db3a45e3c71a
ProyectosMarcos <m-fernandez988@hotmail.com>
2024-03-26 19:36:58 -0300

    mejoras-modal

--- javascript/modal.js
+++ javascript/modal.js
@@ -13,9 +13,32 @@ const btnClose = document
 const modalDos = document.getElementById('dialog_dos');
 
 const btnOpenIniciarCuenta = document
-  .getElementById('open__iniciar-cuenta')
+  .getElementById('open__iniciar-cuenta-2')
   .addEventListener('click', () => modalDos.showModal());
 
 const btnClose2 = document
   .getElementById('button__close-2')
   .addEventListener('click', () => modalDos.close());
+
+/***************/
+
+const modalTres = document.getElementById('dialog-tres');
+
+const btnOpenIniciarCuenta_tres = document
+  .getElementById('open__crear-cuenta-3')
+  .addEventListener('click', () => modalTres.showModal());
+
+const btnClose3 = document
+  .getElementById('button__close-3')
+  .addEventListener('click', () => modalTres.close());
+
+/***************/
+const modalCuatro = document.getElementById('dialog_cuatro');
+
+const btnOpenIniciarCuenta_cuatro = document
+  .getElementById('open__iniciar-cuenta-4')
+  .addEventListener('click', () => modalCuatro.showModal());
+
+const btnClose4 = document
+  .getElementById('button__close-4')
+  .addEventListener('click', () => modalCuatro.close());
